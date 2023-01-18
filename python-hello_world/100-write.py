3f47c4c21f5a4857097b9100287024652cd65f72
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-17 22:49:30 -0500

    Line 4 too long, split message

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-sys.stderr.write("and that piece of are is useful - Dora Kopar, 2015-10-19\n")
+sys.stderr.write("and that piece of are is useful - Dora Kopar, 2\
+015-10-19\n")
 exit (1)
