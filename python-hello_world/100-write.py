0a58bb0071702c58042e5924d72736116b653c71
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-17 22:52:27 -0500

    In line 4, changes are -> art and Kopar -> Korpar

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-sys.stderr.write("and that piece of are is useful - Dora Kopar, 2\
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2\
 015-10-19\n")
 exit (1)
