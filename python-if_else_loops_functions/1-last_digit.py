d610830d9ffed684f2a4395321dd0b1c1603b4a2
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-23 21:35:29 -0500

    Adding if condition to values under 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+if number < 0:
+    last = number % -10
+else:
+    last = number % 10
 if last == 0:
     print(f"Last digit of {number} is {last} and is 0")
 if last > 5:
