9f87e0776653b4c05faecc290e3f4e6a22cd42ea
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-25 23:22:30 -0500

    Adding condition to print argument: when it's only one argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-length = len(sys.argv)
+length = len(sys.argv) - 1
 args = sys.argv
 count = 1
-if length == 1:
-    print("{} arguments.".format(length - 1))
+if length == 0:
+    print("{} arguments.".format(length))
+elif length == 1:
+    print("{} argument:".format(length))
 else:
-    print("{} arguments:".format(length - 1))
-while length > 1:
+    print("{} arguments:".format(length))
+while length > 0:
     for i in args:
-        if count <= length - 1:
+        if count <= length:
             print("{}: {}".format(count, args[count]))
             count = count + 1
         else:
