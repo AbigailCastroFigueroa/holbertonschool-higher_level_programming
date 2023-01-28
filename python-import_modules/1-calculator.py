b84f6c5d7ee3877c9ddcd4d659de95b876cc1d4e
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-27 20:20:28 -0500

    Wrong name, deleted file

--- python-import_modules/1-calculator.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import calculator_1 as calc
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, calc.add(a, b)))
-print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-print("{} / {} = {}".format(a, b, calc.div(a, b)))
