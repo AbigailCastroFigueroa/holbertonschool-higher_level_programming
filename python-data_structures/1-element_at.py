bf325d78ced64b4764092eb1fdbbe4a9d91c44d4
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-29 07:12:19 -0500

    Fixing condition to index out of range

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if 0 < idx > len(my_list):
+    if 0 < idx > len(my_list) - 1:
         return None
     elif idx == 0:
         return my_list[0]
