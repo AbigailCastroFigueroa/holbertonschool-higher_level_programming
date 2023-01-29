c6eefdbd57a3a4b62cc3f50c46da810b6310444e
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-29 09:00:49 -0500

    Adjusting condition to when index is equal 0

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,6 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if 0 < idx > len(my_list) - 1:
         return (my_list)
+    if idx == 0:
+        my_list[0] = element
     for i in my_list:
         if i == idx:
             my_list[i] = element
