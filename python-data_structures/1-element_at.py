6ea0682a2f16e996da7d6e76ecd3fb851a6e7d31
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-29 07:07:55 -0500

    Adding condition to idx == 0

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,6 +2,8 @@
 def element_at(my_list, idx):
     if 0 < idx > len(my_list):
         return None
+    elif idx == 0:
+        return my_list[0]
     for i in my_list:
         if i == idx:
             return my_list[i]
