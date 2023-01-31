3072fd69b63a36174387aec1cd61545b3fd4472a
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-30 22:30:12 -0500

    Adding condition if list in NoneType

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    while length > 0:
-        print("{:d}".format(my_list[length - 1]))
-        length -= 1
+    if my_list is None:
+        return
+    if my_list != 0:
+        length = len(my_list)
+        while length > 0:
+            print("{:d}".format(my_list[length - 1]))
+            length -= 1
