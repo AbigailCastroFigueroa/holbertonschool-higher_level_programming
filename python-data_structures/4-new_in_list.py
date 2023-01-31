2fd82975e3f5a15a7fde70eda9500c7e414aa6e8
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-01-30 20:10:52 -0500

    Uploading the 4-new_in_list.py file

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list) - 1
+    new_list = list.copy(my_list)
+    if 0 > idx > length:
+        return (new_list)
+    else:
+        new_list[idx] = element
+    return (new_list)
