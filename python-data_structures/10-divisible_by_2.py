c49105b3ade9e85ba4842dbb385697b35b3d2ee1
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-01 19:14:01 -0500

    Changing condition in the for loop

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new = list.copy(my_list)
-    for i in new:
-        if i % 2 != 0:
+    for i in range(len(my_list)):
+        if new[i] % 2 != 0:
             new[i] = False
         else:
             new[i] = True
-    i += 1
     return new
