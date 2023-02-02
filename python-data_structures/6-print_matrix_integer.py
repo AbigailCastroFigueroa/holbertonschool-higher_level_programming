5abbfa80ef2cb232ab36723b6616340437d96e0f
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-01 19:21:51 -0500

    Using join function to print whitespaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for var in row:
-            print("{:d} ".format(var), end="")
-        print()
+        print(" ".join("{:d}".format(var) for var in row))
