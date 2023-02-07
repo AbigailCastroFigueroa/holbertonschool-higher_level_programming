980178a76e6b2054deb334e46a6195e385284ce4
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-06 20:57:30 -0500

    Uploading the 1-square.py file

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class module."""
+
+
+class Square:
+    """class Square method."""
+
+    def __init__(self, size):
+        """Documentation __init__ method.
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
