7c2ca2dfcb096e973dce2154c1d7854c636323e2
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-08 22:44:04 -0500

    Removing if condition

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value is int:
-            print("{:d}".format(value))
-            return True
-    except value is not int:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
         return False
