d9444ea4120af09efe8f6be915429ab1d4abfd6e
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-09 22:40:58 -0500

    Uploading the 5-square.py file

---
+++ python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Square class Module."""
+
+
+class Square:
+    """Square class methods."""
+
+    def __init__(self, size=0):
+        """Documentation of __init__ method.
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Documentation of area method.
+
+        Args:
+            self: return the area of the square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print method of the square."""
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print('#', end="")
+            print()
+        if self.__size == 0:
+            print()
