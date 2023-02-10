61ee916e567b6dd6a9c96d87e035466a4d29b616
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-09 20:09:28 -0500

    Uploading the 4-square.py file

---
+++ python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Square class module."""
+
+
+class Square:
+    """Square class methods."""
+
+    def __init__(self, size=0):
+        """Documentation of __init__ method.
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """Getter method."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Documentation of area method.
+
+        Args:
+            self: return the area of the square.
+        """
+        return self.__size * self.__size
