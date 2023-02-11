0ad024c23f4f487af64b53ed5e27630828da8327
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-11 13:38:01 -0500

    Uploading the 0-add_integer.py file

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Addition Module."""
+
+
+def add_integer(a, b=98):
+    """Addition method of two values.
+
+    Args:
+        a: first value.
+        b: second value.    
+    """
+    try:
+        return int(a + b)
+    except TypeError:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        elif not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
