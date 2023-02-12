f4ee8a2a386fbdab5ed5567a68b346d8fec31847
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-11 19:57:23 -0500

    Adding boolean function to if contidion

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    elif a_dictionary == []:
+    elif not bool(a_dictionary):
         return None
     else:
         i = list(a_dictionary.values())
