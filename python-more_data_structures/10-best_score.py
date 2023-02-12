08e84aa9561fb12f086ff006140895c73de3ac85
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-11 19:49:02 -0500

    Adding condition for empty dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    elif a_dictionary == []:
+        return None
     else:
         i = list(a_dictionary.values())
         j = list(a_dictionary.keys())
