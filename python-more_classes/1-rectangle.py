0f1c18fabd27d1753d07eccc408891bc1dd082b7
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-12 07:39:14 -0500

    Adding conditions to the properties decorator methods and the main method

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,29 +15,37 @@ class Rectangle:
         """
         self.__width = width
         self.__height = height
-
-        @property
-        def width(self):
-            """Getter Method."""
-            return self.__width
-
-        @width.setter
-        def width(self, value):
-            self.__width = value
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+            if width < 0:
                 raise ValueError("width must be >= 0")
-
-        @property
-        def height(self):
-            """Getter Method."""
-            return self.__height
-
-        @height.setter
-        def height(self, value):
-            self.__height = value
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value < 0:
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+            if height < 0:
                 raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        """Getter Method."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """Getter Method."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
