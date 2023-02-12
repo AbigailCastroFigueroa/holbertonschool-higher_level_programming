1ba42c3102e51ff6b2d1325019ce249464208e46
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-12 07:42:51 -0500

    Fixing indentation to if condition in the __init__ method.

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,12 +17,12 @@ class Rectangle:
         self.__height = height
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-            if height < 0:
-                raise ValueError("height must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
 
     @property
     def width(self):
