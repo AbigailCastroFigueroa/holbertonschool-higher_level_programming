22b75697d78770c14aff3193529b086653eefef6
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-20 19:45:40 -0500

    Adding documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Class inherit from list class."""
+
+
 class MyList(list):
+    """class MyList."""
+
     def print_sorted(self):
+        """Print a sorted list."""
         new = list.copy(self)
         print(sorted(new))
