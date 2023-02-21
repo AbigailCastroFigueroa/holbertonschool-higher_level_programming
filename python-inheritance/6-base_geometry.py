7ed0ca47ddf8bdc8942c7f24ff6da62d9a8ef81e
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-20 21:58:59 -0500

    Uploading 6-base_geometry.py file

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class module."""
+
+
+class BaseGeometry:
+    """BaseGeometry class methods."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
