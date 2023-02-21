e8b476c09f770823ff53bc740896743d96fdc15e
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-20 21:01:53 -0500

    Removing the comparison with true to the if statement

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,6 @@
 
 def is_kind_of_class(obj, a_class):
     """Return a boolean expression."""
-    if isinstance(obj, a_class) == True:
+    if isinstance(obj, a_class):
         return True
     return False
