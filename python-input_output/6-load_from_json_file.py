2b7002c81f9aec9ccc55001fcb1e734a937630d5
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-22 22:54:11 -0500

    Uploading 6-load_from_json_file.py file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create an object from a JSON file."""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create an object from a JSON file."""
+    with open(filename, 'r') as file:
+        return json.load(file)
