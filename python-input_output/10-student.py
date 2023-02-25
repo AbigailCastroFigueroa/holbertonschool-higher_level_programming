16e60e0969badfa8751e7ed301c86b907019cf6f
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-02-25 12:45:16 -0500

    Uploading the 10-student.py file

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student class module."""
+
+import json
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Student class methods."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary."""
+        dict_to_return = dict()
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            for i in attrs:
+                if i in self.__dict__:
+                    dict_to_return.update({i: self.__dict__[i]})
+            return dict_to_return
+        return self.__dict__.copy()
