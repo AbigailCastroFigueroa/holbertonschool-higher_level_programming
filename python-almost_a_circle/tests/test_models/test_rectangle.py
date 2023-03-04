12688af9c5003301a68280366c0dac13802f4e2d
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-03-04 16:41:12 -0500

    Uploading the rectangle test file

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Rectangle class tests."""
+
+import unittest
+import os
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class RectangleTest(unittest.TestCase):
+    """Test for the rectangle class methods."""
+
+    def test_instance(self):
+        r = Rectangle(1, 2)
+        self.assertEqual(isinstance(r, Base), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
