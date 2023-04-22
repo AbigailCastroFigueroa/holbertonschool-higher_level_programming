45fcf892978bdb6cb6cfd213c5b7d49bdfc050ed
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-22 16:17:19 -0400

    Removing database name from line 3

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Create a table in a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'htbn_0d_usa'.'states' (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
   id NOT NULL UNIQUE AUTO_INCREMENT,
   name VARCHAR(256),
   PRIMARY KEY (id)
