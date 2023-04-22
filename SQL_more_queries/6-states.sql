b690d7bca4b5a3a2fbb61516450aac385bc9ae42
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-22 16:28:44 -0400

    Adding not null condition to the name column

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
   id NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256),
+  name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (id)
 );
