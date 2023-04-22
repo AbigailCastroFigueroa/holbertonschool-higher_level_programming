409cab085b54458e5dab62f11d612dab02fc6d46
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-22 17:11:06 -0400

    Addding data type id value

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Create a table in a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-  id NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id)
 );
