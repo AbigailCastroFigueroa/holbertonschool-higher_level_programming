382b5b9b487fd9c4625323ae7e5729ffc5d264ef
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-22 17:02:15 -0400

    Adding auto increment to id line 4

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Creates a database and create a table in the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   state_id INT NOT NULL,
   name VARCHAR(256) NOT NULL,
   PRIMARY KEY (id),
