354a7cc68405fa0b706d9f49411f4a89aba88637
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-22 10:13:31 -0400

    changing syntax

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Give the average of all records in a table of the database
-SELECT * FROM second_table WHERE average > ( SELECT AVG(score) FROM second_table);
+SELECT AVG(score) As average FROM second_table;
 
