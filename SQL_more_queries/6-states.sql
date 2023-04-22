d028690421b586809dc22d1e225448e63dee51a6
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-22 16:15:38 -0400

    adding single quotes in line 3

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create a table in a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS 'htbn_0d_usa'.'states' (
   id NOT NULL UNIQUE AUTO_INCREMENT,
   name VARCHAR(256),
   PRIMARY KEY (id)
