623640df2b0026c2706058986f0da9cf39a2c007
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-22 16:45:07 -0400

    Adding database in same line as table name

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
 -- Creates a database and create a table in the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
   id INT NOT NULL,
   state_id INT NOT NULL,
   name VARCHAR(256) NOT NULL,
