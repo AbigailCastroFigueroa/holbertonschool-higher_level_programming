10e4f13a442d675ac2a7f72a67c949de22d83fdd
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-26 20:54:21 -0400

    Uploading the 13-count_shows_by_genre.sql file

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Count how many films belongs to the same genre.
+SELECT name AS "genre", COUNT(tv_show_genres.show_id) AS "number_of_shows"
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC;
+
