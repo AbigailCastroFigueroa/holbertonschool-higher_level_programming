7eb6c638c771357704aa4fe15e0fb26e3190fdf4
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-26 21:55:20 -0400

    Uploading the 16-show_by_genre.sql file

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Show the title and the different genre to which the film belongs
+SELECT title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC;
+
