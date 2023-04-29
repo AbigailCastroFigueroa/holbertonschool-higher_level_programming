0955097f66c8d12d1ec409ec6687d39b30979962
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-04-29 15:45:20 -0400

    Fixing length line 17, pycodestyle

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,8 +14,9 @@ if __name__ == '__main__':
 
     cur = working_database.cursor()
     state = sys.argv[4]
-    query = f"SELECT cities.name FROM cities LEFT JOIN states ON cities.state_id=\
-        states.id WHERE states.name='{state}' ORDER BY cities.id ASC"
+    query = f"SELECT cities.name FROM cities\
+          LEFT JOIN states ON cities.state_id=states.id\
+          WHERE states.name='{state}' ORDER BY cities.id ASC"
     cur.execute(query)
     if cur:
         print(", ".join(i[0] for i in cur))
