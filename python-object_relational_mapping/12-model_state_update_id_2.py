fb394f0823cd768bf80ec2ba317d30ada2993184
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-05-06 22:52:41 -0400

    Uploading the 12-model_state_update_id_2.py file

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Scrip to perform an update to a database."""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data_to_update = session.query(State).filter(State.id == 2)
+    data_to_update.name = 'Canada'
+    session.commit()
