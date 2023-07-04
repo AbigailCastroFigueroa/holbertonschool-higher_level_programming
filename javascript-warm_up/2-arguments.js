887fbb6509ed2747bb39fdc9b50e0bd6d8dcfa8f
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-04 17:07:43 -0400

    Removing 'arguments' word as variable name, apparently it's not allowed.

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 const process = require('process');
-const arguments = process.argv;
-let amount_of_arguments = arguments.length
-if (amount_of_arguments === 3) {
-    console.log('Argument found');
-}
-else if (amount_of_arguments > 3) {
-    console.log('Arguments found');
-}
-else {
-    console.log('No Argument');
+const line = process.argv;
+const words = line.length;
+if (words === 3) {
+  console.log('Argument found');
+} else if (words > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No Argument');
 }
