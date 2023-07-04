26d41356054357da8db2a1fa61fcf493b7ec7eee
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-04 17:37:11 -0400

    Uploading the 6-multi_languages_loop.js file

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const length = words.length;
+for (let index = 0; index < length; index++) {
+  console.log(words[index]);
+}
