fd81be37aaaecc4f9ec004d321c4d1cf258cbc1c
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-08 11:06:08 -0400

    typo

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const process = require('process');
-const words = process.argv.lenght - 2;
+const words = process.argv.length - 2;
 if (words === 0) {
   console.log('No Argument');
 } else if (words === 1) {
