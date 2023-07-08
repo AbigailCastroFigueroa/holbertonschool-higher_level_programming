d35c7a7dad6baddfc9d82a162d30c8f2195abdaf
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-08 10:56:37 -0400

    Rearranging the order of the conditions

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 const process = require('process');
 const line = process.argv;
 const words = line.length;
-if (words === 3) {
-  console.log('Argument found');
-} else if (words > 3) {
-  console.log('Arguments found');
-} else {
+if (words < 3) {
   console.log('No Argument');
+} else if (words === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
