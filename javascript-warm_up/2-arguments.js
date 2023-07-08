c8e7b61ce0ced33be321ff609262b77fe01f260b
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-08 11:00:01 -0400

    Changing the first if condition again

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const line = process.argv;
 const words = line.length;
-if (words < 3) {
+if (words <= 2) {
   console.log('No Argument');
 } else if (words === 3) {
   console.log('Argument found');
