73413a969a83b68ec01163d98caf4f4b2e3b7710
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-10 22:28:17 -0400

    Uploading the 0-readme.js file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const file = process.argv[2];
+const fs = require('fs');
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
