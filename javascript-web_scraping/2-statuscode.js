93686b493c284e0a360c23eb56c639d705e4b2c2
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-10 23:22:25 -0400

    Uploading the 2-statuscode.js file

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url).on('response', function (response) {
+  console.log('code: ', response.statusCode.toString());
+});
