6e70873b14e6f96376ffa0e650404ea14d81b774
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-10 23:43:12 -0400

    Removing the extra whitespace in the output

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 const url = process.argv[2];
 request(url).on('response', function (response) {
-  console.log('code: ', response.statusCode.toString());
+  console.log('code:', response.statusCode.toString());
 });
